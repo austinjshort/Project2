2421cea4c8d189d036f4f6fa54fcf258f8e37abf
austinjshort <50437454+austinjshort@users.noreply.github.com>
2020-02-27 19:16:18 -0800

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,5 +1,34 @@
 public class Course {
 
-    //can ya'll see this?
-    
+    private int courseCapacity, waitListCapacity;
+    private Student[] enrolledStudents, waitList;
+
+    public Course(Student[] enrolledStudents, int courseCapacity, Student[] waitList, int waitListCapacity){
+        this.courseCapacity = courseCapacity;
+        this.waitListCapacity = waitListCapacity;
+        this.waitList = new Student[waitListCapacity];
+        this.enrolledStudents = new Student[courseCapacity];
+    }
+
+    public boolean dropStudent(){
+
+    }
+
+
+    public boolean addStudent(){
+
+    }
+
+    public static int nullCounter(String[] wordArray, int startPoint){
+        if (startPoint >= wordArray.length){ return 0; } //input validation .
+
+        int nullCount = 0;
+        for (int count = startPoint; count < wordArray.length; count++) {
+            String word = wordArray[count];
+            if (word == null) {
+                nullCount++;
+            }
+        }
+        return nullCount;
+    }
 }
